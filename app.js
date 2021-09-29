e24f484ebacb468215827299dcef50c576efcaeb
netoxdw <neto.guevara.tapia@gmail.com>
2021-09-29 16:28:46 -0500

    Variables y prompt

---
+++ app.js
@@ -0,0 +1,10 @@
+// let boleta = prompt('Ingreasa boleta');
+// alert(boleta);
+
+// concatenar
+let saludo = 'Hola';
+let Pregunta = '¿como te llamas?';
+
+var frase = saludo + Pregunta;
+
+document.write(frase)
